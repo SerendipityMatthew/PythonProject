c5282c5aa08e9b7a0e1db28cc0d7eb76874376c0
xuwanjin <xuwanjin1991@Gmail.com>
2018-04-18 18:09:22 +0800

    add statistic for time
    Signed-off-by: xuwanjin <xuwanjin1991@Gmail.com>

--- pocket_article_statistic/pocket_analysis.py
+++ pocket_article_statistic/pocket_analysis.py
@@ -3,6 +3,7 @@ import time
 from collections import Counter
 
 import matplotlib.pyplot as plt
+import numpy as np
 from bs4 import BeautifulSoup
 
 pocket_export = open("ril_export.html", 'r')
@@ -39,27 +40,54 @@ class Article:
 
 
 lists = map(parse_article, article_list)
-article_time_list = []
+article_date_list = []  # 2018-03
+article_time_list = []  # 12:24
 for l in lists:
     time_string = l.time_added
-    article_time_list.append(str(time_string)[0:7])
+    article_date_list.append(str(time_string)[0:7])
+    article_time_list.append(int(str(time_string)[11:13]))
 
+article_date_list = Counter(article_date_list)
 article_time_list = Counter(article_time_list)
 
-x_time = [k for k in article_time_list.keys()]
+x_time = [k for k in article_date_list.keys()]
 x = [k for k in reversed(x_time)]
-y_count = [v for v in article_time_list.values()]
-y = [y for y in reversed(y_count)]
+y_count = [v for v in article_date_list.values()]
+y = [v for v in reversed(y_count)]
 
-print(x)
-print(y)
+
+def dict2list(dic: dict):
+    keys = dic.keys()
+    vals = dic.values()
+    lst = [(key, val) for key, val in zip(keys, vals)]
+    return lst
+
+
+article_time_list = sorted(dict2list(article_time_list), key=lambda x: x[0])
+article_time_list = dict(article_time_list)
+time_x = [k for k in article_time_list.keys()]
+time_y = [v for v in article_time_list.values()]
+
+# print(time_x)
+# print(time_y)
 
 plt.figure(figsize=(200, 100))
 plt.title("Saved Article Statistics")
 plt.xlabel('Date of saved article')
 plt.ylabel('Article Account')
-plt.plot(x, y, 'r')
+plt.plot(x, y, 'g', linewidth=2, )
 plt.grid(True)
 plt.legend()
 plt.xticks(rotation=45)
+plt.show()
+
+plt.figure(figsize=(200, 100))
+plt.title("Saved Article Statistics")
+plt.xlabel('Time of saved article')
+plt.ylabel('Article Account')
+plt.plot(time_x, time_y, 'r', linewidth=2, )
+plt.grid(True)
+plt.legend()
+plt.xticks(rotation=45)
+plt.xticks(np.arange(min(time_x), max(time_x) + 1, 1.0))
 plt.show()
